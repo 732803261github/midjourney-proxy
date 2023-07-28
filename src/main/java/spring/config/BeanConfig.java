aeb88a6e588e35199c2963366641ed53315b801e
chenxiaoming <732803261@qq.com>
2023-07-28 13:40:16 +0800

    序列化改成jackson2JsonRedisSerializer

--- src/main/java/spring/config/BeanConfig.java
+++ src/main/java/spring/config/BeanConfig.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -48,8 +49,11 @@ public class BeanConfig {
 	@Bean
 	RedisTemplate<String, Task> taskRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, Task> redisTemplate = new RedisTemplate<>();
+		Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
 		redisTemplate.setConnectionFactory(redisConnectionFactory);
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
+//		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 		return redisTemplate;
 	}
